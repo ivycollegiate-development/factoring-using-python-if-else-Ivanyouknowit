24d2ad5593f4ac0e8f199eaacddbb34fcd4d263c
Ivanyouknowit <iku26_student@ivycollegiate.org>
2024-10-04 07:54:35 +0000

    Wrotes logic to check for factors

--- Ivan_Sprint_Projectv2.75/Ivan_Sprint_Projectv2.75.py
+++ Ivan_Sprint_Projectv2.75/Ivan_Sprint_Projectv2.75.py
@@ -1,1 +1,6 @@
-#f inding factors of values
+# finding factors of values
+def = is_factor (a, b):
+    if b % a == 0: # This is the modelo operator
+        return True 
+    else:
+        return False
